9bb17efd923c83a7a20a486658888b1af08fbedc
Maricruz <gil.maricruz@gmail.com>
2016-07-03 15:22:20 -0300

    se agrega el listado pero tiene problemas

--- RecuCIU/src/main/webapp/javascripts/controllers/GastosController.js
+++ RecuCIU/src/main/webapp/javascripts/controllers/GastosController.js
@@ -1,10 +1,27 @@
-controlGastos.controller("GastosController", function ($state, $scope) {
+//controlGastos.controller("GastosController", function ($state, $scope, gastosService) {
+controlGastos.controller("GastosController", ['$scope', function ($scope, gastosService,$state) {
+	
+  var self = this;
 
+    $scope.gastos = []
+
+    var self = this;
     $scope.ingresar = function() {
         $state.go("gastos");
     }
+/*
+    self.gastos = [
+       {
+            mes: "Alfreds Futterkiste",
+            descripcion : "Germany",
+            monto : "Germany"
+        }, 
 
-    
-
-});
-
+        {
+            mes: "Alfreds Futterkiste",
+            descripcion : "Germany",
+            monto : "Germany"
+        }
+    ]
+*/
+}]);
